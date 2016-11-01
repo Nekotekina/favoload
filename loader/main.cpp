2855ad85c602c105d3067ae297bae81c835e0e6f
Nekotekina <nekotekina@gmail.com>
2016-11-01 16:24:54 +0300

    +OpenDialog

--- loader/main.cpp
+++ loader/main.cpp
@@ -1,12 +1,7 @@
-#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <shellapi.h>
 
-int main()
+int main(int argc, wchar_t* argv[])
 {
-	int       argc = 0;
-	wchar_t** argv = CommandLineToArgvW(GetCommandLineW(), &argc);
-
 	const wchar_t* exe = argc >= 2 ? argv[1] : nullptr;
 	const wchar_t* dll = argc >= 3 ? argv[2] : L"payload.dll";
 
@@ -34,49 +29,81 @@ int main()
 		}
 	}
 
+	wchar_t buf[256]{};
+
 	if (!exe)
 	{
-		::MessageBoxW(0, L"No supported executable found.", L"<empty cmd line>", MB_ICONERROR);
-		::ExitProcess(1);
+		::MessageBoxW(0, L"No supported executable found.", L"<empty cmd line>", MB_ICONWARNING);
+
+		wchar_t dir[256]{};
+		::GetCurrentDirectoryW(sizeof(dir), dir);
+
+		::OPENFILENAMEW ofn{};
+		ofn.lStructSize     = sizeof(ofn);
+		ofn.lpstrFilter     = L"*.exe\0*.exe\0";
+		ofn.lpstrFile       = buf;
+		ofn.nMaxFile        = sizeof(buf);
+		ofn.Flags           = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+		ofn.lpstrTitle      = L"Select executable file";
+		ofn.lpstrInitialDir = dir;
+
+		if (!::GetOpenFileNameW(&ofn))
+		{
+			return 0;
+		}
+
+		exe = buf;
 	}
 
-	::STARTUPINFOW start;
-	::RtlSecureZeroMemory(&start, sizeof(start));
+	::STARTUPINFOW start{};
 	start.cb = sizeof(start);
-	::PROCESS_INFORMATION out;
+	::PROCESS_INFORMATION out{};
 
 	if (!::CreateProcessW(exe, argc >= 4 ? argv[3] : nullptr, nullptr, nullptr, false, CREATE_SUSPENDED, nullptr, nullptr, &start, &out))
 	{
 		::MessageBoxW(0, L"Failed to create a process.", exe, MB_ICONERROR);
-		::ExitProcess(1);
+		return 1;
 	}
 
-	LPVOID dll_name = ::VirtualAllocEx(out.hProcess, NULL, 4096, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	auto dll_name = ::VirtualAllocEx(out.hProcess, NULL, 4096, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 
 	if (!dll_name || !::WriteProcessMemory(out.hProcess, dll_name, dll, ::lstrlenW(dll) + sizeof(wchar_t), nullptr))
 	{
 		::MessageBoxW(0, L"Failed to write process memory.", exe, MB_ICONERROR);
-		::ExitProcess(1);
+		return 1;
 	}
 
-	auto dll_entry = reinterpret_cast<::PTHREAD_START_ROUTINE>(&LoadLibrary);
+	auto thread = ::CreateRemoteThread(out.hProcess, nullptr, 0, reinterpret_cast<::PTHREAD_START_ROUTINE>(&LoadLibrary), dll_name, 0, nullptr);
 
-	HANDLE rmt = ::CreateRemoteThread(out.hProcess, nullptr, 0, dll_entry, dll_name, 0, nullptr);
-
-	if (rmt == INVALID_HANDLE_VALUE)
+	if (thread == INVALID_HANDLE_VALUE)
 	{
 		::MessageBoxW(0, L"Failed to create remote thread.", exe, MB_ICONERROR);
-		::ExitProcess(1);
+		return 1;
 	}
 
 	// Wait for remote thread
-	::WaitForSingleObject(rmt, INFINITE);
+	::WaitForSingleObject(thread, INFINITE);
 
 	// Cleanup
-	::CloseHandle(rmt);
+	::CloseHandle(thread);
 	::VirtualFreeEx(out.hProcess, dll_name, 0, MEM_RELEASE);
 
 	// Finally launch the process
 	::ResumeThread(out.hThread);
-	::ExitProcess(0);
+	return 0;
+}
+
+void startup()
+{
+	auto argc = 0;
+	auto argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
+	::ExitProcess(::main(argc, argv));
+}
+
+#pragma function(memset)
+void*__cdecl memset(void* dst, int val, size_t size)
+{
+	auto ptr = static_cast<char*>(dst);
+	while (size--) *ptr++ = static_cast<char>(val);
+	return dst;
 }
--- payload/dllmain.cpp
+++ payload/dllmain.cpp
@@ -1,15 +1,29 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-static void redirect(LPCSTR dll, LPCSTR func, DWORD target)
+static void redirect(LPCSTR dll, LPCSTR func, LPVOID ptr)
 {
-	BYTE* proc = (BYTE*)::GetProcAddress(::GetModuleHandleA(dll), func);
-	target -= DWORD(proc) + 5;
+	const auto data = reinterpret_cast<BYTE*>(::GetProcAddress(::GetModuleHandleA(dll), func));
+	const auto target = reinterpret_cast<BYTE*>(ptr) - data - 5;
+
 	DWORD old_protect;
-	::VirtualProtect(proc, 8, PAGE_EXECUTE_READWRITE, &old_protect);
-	proc[0] = 0xe9;
-	::CopyMemory(proc + 1, &target, 4);
-	::VirtualProtect(proc, 8, old_protect, NULL);
+	::VirtualProtect(data, 2 * sizeof(void*), PAGE_EXECUTE_READWRITE, &old_protect);
+
+	if (target > 0x7fffffff || target < -0x80000000ll)
+	{
+		data[0] = 0x48; // MOV rax, imm64
+		data[1] = 0xb8;
+		::CopyMemory(data + 2, &ptr, sizeof(ptr));
+		data[10] = 0xff; // JMP rax
+		data[11] = 0xe0;
+	}
+	else
+	{
+		data[0] = 0xe9; // JMP rel32
+		::CopyMemory(data + 1, &target, 4);
+	}
+
+	::VirtualProtect(data, 2 * sizeof(void*), old_protect, NULL);
 }
 
 static int WINAPI lstrcmpiA_(LPCSTR s1, LPCSTR s2)
@@ -23,7 +37,7 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID)
     {
     case DLL_PROCESS_ATTACH:
 	{
-		redirect("KERNEL32.dll", "lstrcmpiA", (DWORD)&lstrcmpiA_);
+		redirect("KERNEL32.dll", "lstrcmpiA", lstrcmpiA_);
 		break;
 	}
     case DLL_THREAD_ATTACH:
